5de9a37e3b4067e8843bdcf33fd667daedc11908
sebastiananea <51966711+sebastiananea@users.noreply.github.com>
2021-07-19 11:14:24 -0300

    Add files via upload

---
+++ precache-manifest.c367d43e5ebcc762b56f3c0b2df83000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee3cadf2336cc3035c32fce2420a582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31f4fc0da07a600f3722",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f4fc0da07a600f3722",
+    "url": "/static/js/main.10687bfe.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
